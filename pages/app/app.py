576a73a1abf85ae8527a48c035a3b1b39cf65852
felipealhante <alhantefelipe@gmail.com>
2026-01-22 12:30:17 +0100

    Delete pages/app directory

--- pages/app/app.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="Quantify", layout="wide")
-
-st.title("Quantify")
-st.write("Use the sidebar to navigate between tools.")
--- pages/app/pages/2_Monte_Carlo.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.title("Monte Carlo Simulation")
-st.write("Monte Carlo page")
