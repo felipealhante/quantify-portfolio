72bd7f0ecad965b996719482509a5e3bd5dd2091
felipealhante <alhantefelipe@gmail.com>
2026-01-27 15:34:56 +0100

    Update and rename treinao.py to Welcome_Page.py

---
+++ Welcome_Page.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Quantitative Projects",
+    layout="wide"
+)
+
+st.title("Quantitative Projects")
+st.write("Select a project from the sidebar.")
